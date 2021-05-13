12149e7f6c295450d115625fcc4a7738a041addd
rlaqudwls25 <rlaqudwls157@gmail.com>
2021-05-13 13:08:27 +0900

    rootReducer를 통해 state 업데이트 해줄 수 있게 store 생성 후 provider 적용

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
 import GlobalStyle from "./Styles/GlobalStyle";
+import { Provider } from "react-redux";
+import { createStore } from "react-redux";
+import rootReducer from "./store/reducers/";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <Routes />
+    <Provider store={store}>
+      <GlobalStyle />
+      <Routes />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
